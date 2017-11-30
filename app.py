fb741cfb31ac27a9a147f195d27dfabd09295705
surfer190 <shutch190@gmail.com>
2017-11-30 11:11:14 +0200

    Add flot chart

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import datetime, calendar
 from flask import Flask, g, render_template
 
 import models
@@ -19,12 +20,28 @@ def after_request(response):
 
 
 def get_data():
-    data = models.PeopleServed.select().dicts()
+    data = models.PeopleServed.select().order_by(models.PeopleServed.date.desc()).limit(5).dicts()
     people_list = []
     for item in data:
-        people_list.append({'data': str(item['date']), 'people': item['people']})
+        people_list.append({'date': str(item['date']), 'people': item['people']})
     return people_list
 
+def get_list_data():
+    data = models.PeopleServed.select().order_by(models.PeopleServed.date.desc()).limit(5).dicts()
+    people_list = []
+    for item in data:
+        people_list.append([str(item['date']), item['people']])
+    return people_list
+
+
+def get_flot_data():
+    '''Flot requires unix timestamps'''
+    data = models.PeopleServed.select().order_by(models.PeopleServed.date.desc()).limit(15).dicts()
+    people_list = []
+    for item in data:
+        time = calendar.timegm(item['date'].timetuple()) * 1000
+        people_list.append([time, item['people']])
+    return people_list
 
 @app.route('/d3js')
 def d3js():
@@ -46,7 +63,7 @@ def matplotlib():
 
 @app.route('/flot')
 def flot():
-    data = get_data()
+    data = get_flot_data()
     return render_template('flot.html', data=data)
 
 
